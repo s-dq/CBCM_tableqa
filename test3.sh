81b241d22c43c995a95d8436e3662376544490d9
s-dq <189148875@qq.com>
2024-12-18 13:17:39 +0800

    Update and rename a.py to test3.sh

---
+++ test3.sh
@@ -0,0 +1,29 @@
+mkdir -p ./result/test3/cells_ernie_base
+mkdir -p ./result/test3/apply/cells
+
+CUDA_VISIBLE_DEVICES=0 python ./IM-TQA-main/TQA_code/train_RCI_model.py \
+--model_type ernie \
+--do_lower_case \
+--train_dir ./traindata/test3/train_cells.jsonl.gz \
+--dev_dir ./traindata/test3/test_cells.jsonl.gz \
+--seed 1234 \
+--full_train_batch_size 64 \
+--gradient_accumulation_steps 8 \
+--num_train_epochs 1 \
+--learning_rate 1e-5 \
+--warmup_fraction 0.1 \
+--train_instances 150193 \
+--weight_decay 0.01 \
+--max_seq_length 512 \
+--output_dir ./result/test3/cells_ernie_base
+
+
+CUDA_VISIBLE_DEVICES=0 python ./IM-TQA-main/TQA_code/apply_RCI_model.py \
+--model_type ernie \
+--model_name_or_path ./result/test3/cells_ernie_base \
+--do_lower_case \
+--input_dir ./traindata/test3/test_cells.jsonl.gz \
+--max_seq_length 512 \
+--output_dir ./result/test3/apply/cells
+
+python compute_CI_exact_match.py "./result/test3/apply/cells"
